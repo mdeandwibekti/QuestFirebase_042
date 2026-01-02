10e007b6e215225fd7c3b3f3dc2d63a3fcbd266e
mdeandwibekti <mdeandwibekti@gmail.com>
2026-01-02 16:44:44 +0700

    Menambahkan DestinasiEntry untuk navigasi ke halaman input data siswa

---
+++ app/src/main/java/com/example/firebase_042/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.firebase_042.view.route
+
+import com.example.firebase_042.R
+import com.example.firebase_042.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}

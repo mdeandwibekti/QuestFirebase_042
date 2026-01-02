a70cc9aa5bc16ee86bd8a8c5867f9793773adaf8
mdeandwibekti <mdeandwibekti@gmail.com>
2026-01-02 16:33:55 +0700

    Menambahkan data class Siswa dan DetailSiswa sebagai model data siswa

---
+++ app/src/main/java/com/example/firebase_042/modeldata/Siswa.kt
@@ -0,0 +1,16 @@
+package com.example.firebase_042.modeldata
+
+data class Siswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+data class DetailSiswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+

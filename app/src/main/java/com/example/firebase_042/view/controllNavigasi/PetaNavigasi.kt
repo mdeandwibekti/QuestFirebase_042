ffa777c0595583100547d36008defd792029533c
mdeandwibekti <mdeandwibekti@gmail.com>
2026-01-02 16:42:19 +0700

    Menambahkan pengaturan navigasi aplikasi data siswa menggunakan Jetpack Compose

---
+++ app/src/main/java/com/example/firebase_042/view/controllNavigasi/PetaNavigasi.kt
@@ -0,0 +1,41 @@
+package com.example.firebase_042.view.controllNavigasi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.firebase_042.view.EntrySiswaScreen
+import com.example.firebase_042.view.HomeScreen
+import com.example.firebase_042.view.route.DestinasiDetail
+import com.example.firebase_042.view.route.DestinasiEntry
+import com.example.firebase_042.view.route.DestinasiHome
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(
+                DestinasiEntry
+                .route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)
+            })
+        }
+    }
+}

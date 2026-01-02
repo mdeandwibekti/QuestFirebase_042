7ce717bf40618cf7fc04ea4f4b1a60e68ea7d81b
mdeandwibekti <mdeandwibekti@gmail.com>
2026-01-02 16:45:47 +0700

    Menambahkan DestinasiHome sebagai halaman utama pada sistem navigasi aplikasi

---
+++ app/src/main/java/com/example/firebase_042/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.firebase_042.view.route
+
+import com.example.firebase_042.R
+import com.example.firebase_042.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}

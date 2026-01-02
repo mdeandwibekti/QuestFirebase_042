37f369b48d5d69949ca8ab6dc8e044f07af7a529
mdeandwibekti <mdeandwibekti@gmail.com>
2026-01-02 17:11:46 +0700

    menambahkan LoadingScreen dan ErrorScreen sebagai komponen UI untuk menampilkan indikator proses pemuatan data serta penanganan kondisi gagal, sehingga pengguna mendapatkan umpan balik yang jelas saat data sedang dimuat atau terjadi kesalahan.

--- app/src/main/java/com/example/firebase_042/view/HalamanHome.kt
+++ app/src/main/java/com/example/firebase_042/view/HalamanHome.kt
@@ -93,3 +93,32 @@ fun HomeBody(
     }
 }
 
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img__1_),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun ErrorScreen(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(R.string.gagal))
+        Button(
+            onClick = retryAction,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+

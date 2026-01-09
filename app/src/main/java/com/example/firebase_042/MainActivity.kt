6539c2274c162bd7ddce182f12c338c1c81d3eb3
mdeandwibekti <mdeandwibekti@gmail.com>
2026-01-09 20:04:22 +0700

    Menambahkan konfigurasi setContent menggunakan Firebase_042Theme dan Scaffold sebagai kerangka utama UI, serta memuat DataSiswaApp dengan pengaturan padding yang sesua

---
+++ app/src/main/java/com/example/firebase_042/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.firebase_042
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.firebase_042.ui.theme.Firebase_042Theme
+import com.example.firebase_042.view.controllNavigasi.DataSiswaApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Firebase_042Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DataSiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Firebase_042Theme {
+        Greeting("Android")
+    }
+}
